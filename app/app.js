d4a5af940ad30ae1d63e276b14cc377baeb0a250
Nick Hu <nick@511.pm>
2016-02-22 22:00:51 -0800

    * Authentication * User * Channels * Channels/Messages

--- app/app.js
+++ app/app.js
@@ -18,17 +18,105 @@ angular
     $stateProvider
       .state('home', {
         url: '/',
-        templateUrl: 'home/home.html'
+        templateUrl: 'home/home.html',
+        resolve: {
+          requireNoAuth: function($state, Auth){
+            return Auth.$requireAuth().then(function(auth){
+              $state.go('channels');
+            }, function(error){
+              return;
+            });
+          }
+        }
       })
       .state('login', {
         url: '/login',
-        templateUrl: 'auth/login.html'
+        controller: 'AuthCtrl as authCtrl',
+        templateUrl: 'auth/login.html',
+        resolve: {
+          requireNoAuth: function($state, Auth){
+            return Auth.$requireAuth().then(function(auth){
+              $state.go('home');
+            }, function(error){
+              return;
+            });
+          }
+        }
       })
       .state('register', {
         url: '/register',
-        templateUrl: 'auth/register.html'
+        controller: 'AuthCtrl as authCtrl',
+        templateUrl: 'auth/register.html',
+        resolve: {
+          requireNoAuth: function($state, Auth){
+            return Auth.$requireAuth().then(function(auth){
+              $state.go('home');
+            }, function(error){
+              return;
+            });
+          }
+        }
+      })
+      .state('profile', {
+        url: '/profile',
+        controller: 'ProfileCtrl as profileCtrl',
+        templateUrl: 'users/profile.html',
+        resolve: {
+          auth: function($state, Users, Auth){
+            return Auth.$requireAuth().catch(function(){
+              $state.go('home');
+            });
+          },
+          profile: function(Users, Auth){
+            return Auth.$requireAuth().then(function(auth){
+              return Users.getProfile(auth.uid).$loaded();
+            });
+          }
+        }
+      })
+      .state('channels', {
+        url: '/channels',
+        controller: 'ChannelsCtrl as channelsCtrl',
+        templateUrl: 'channels/index.html',
+        resolve: {
+          channels: function(Channels){
+            return Channels.$loaded();
+          },
+          profile: function($state, Auth, Users){
+            return Auth.$requireAuth().then(function(auth){
+              return Users.getProfile(auth.uid).$loaded().then(function(profile){
+                if (profile.displayName){
+                  return profile;
+                }else{
+                  $state.go('profile');
+                }
+              })
+            }, function(error){
+              $state.go('home');
+            });
+          }
+        }
+      })
+      .state('channels.create', {
+        url: '/create',
+        templateUrl: 'channels/create.html',
+        controller: 'ChannelsCtrl as channelsCtrl'
+      })
+      .state('channels.messages', {
+        url: '/{channelId}/messages',
+        templateUrl: 'channels/messages.html',
+        controller: 'MessagesCtrl as messagesCtrl',
+        resolve: {
+          messages: function($stateParams, Messages){
+            return Messages.forChannel($stateParams.channelId).$loaded();
+          },
+          channelName: function($stateParams, channels){
+            return '#' + channels.$getRecord($stateParams.channelId).name;
+          }
+        }
       });
 
     $urlRouterProvider.otherwise('/');
   })
-  .constant('FirebaseUrl', 'https://slack.firebaseio.com/');
+
+  .constant('FirebaseUrl', 'https://fireslack3.firebaseio.com/');
---
+++ app/channels/channels.controller.js
@@ -0,0 +1,28 @@
+angular.module('angularfireSlackApp')
+	.controller('ChannelsCtrl', function($state, Auth, Users, profile, channels){
+
+		var channelsCtrl = this;
+
+		channelsCtrl.newChannel = {
+			name: ''
+		};
+
+		channelsCtrl.profile = profile;
+		channelsCtrl.channels = channels;
+		channelsCtrl.getDisplayName = Users.getDisplayName;
+		channelsCtrl.getGravatar = Users.getGravatar;
+
+
+		channelsCtrl.createChannel = function(){
+			channelsCtrl.channels.$add(channelsCtrl.newChannel).then(function(ref){
+				$state.go('channels.messages', {channelId: ref.key()});
+			});
+		}
+
+		channelsCtrl.logout = function(){
+			Auth.$unauth();
+			$state.go('home');
+		}
+
+
+	});
